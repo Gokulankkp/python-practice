799b7ac7624f0d9b3d79b2aeac84a85c9f90e7d3
gokul kp <gokulankkp@gmail.com>
2021-07-31 13:23:49 +0530

    prctice

---
+++ area of t.py
@@ -0,0 +1,3 @@
+b = float(input('enter base of a triangle: '))
+h = float(input('enter height of a triangle: '))
+area = (b*h)/2
---
+++ high and low.py
@@ -0,0 +1,3 @@
+def high_and_low(numbers):
+     return max(numbers), min(numbers)
+print(high_and_low("12345"))
